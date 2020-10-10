c36d856f7e5dadebcb421e73c31bd0061748c09f
Matías Molleja <matiasmolleja@outlook.com>
2020-10-10 14:19:14 +0200

    Initial commit with default empty app.

---
+++ EngraveCenterer/EngraveCenterer/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using EngraveCenterer.ViewModels;
+using EngraveCenterer.Views;
+using Xamarin.Forms;
+
+namespace EngraveCenterer
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ EngraveCenterer/EngraveCenterer/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using EngraveCenterer.ViewModels;
+
+namespace EngraveCenterer.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
